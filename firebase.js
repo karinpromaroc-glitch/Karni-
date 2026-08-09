c6b0340ed98b117ea9309d33ea07af4e682e7217
karinpromaroc-glitch <karinpromaroc@gmail.com>
2026-08-09 15:55:36 +0100

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,20 @@
+// firebase.js - Karni Pro - الإعداد الرسمي الموحد
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCLIzcO9i8QQ-YzYY5fIap6QNDsJDqgf60",
+  authDomain: "karnipromaroc.firebaseapp.com",
+  databaseURL: "https://karnipromaroc-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "karnipromaroc",
+  storageBucket: "karnipromaroc.firebasestorage.app",
+  messagingSenderId: "6746147726",
+  appId: "1:6746147726:web:6e3a2d29941ab93ef432df",
+  measurementId: "G-3Q1L0LC9TE"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getDatabase(app);
+export default app;
