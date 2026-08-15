9841e9e2256f9948393cad273cb815edf153b415
karinpromaroc-glitch <karinpromaroc@gmail.com>
2026-08-15 19:46:49 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,19 +1,20 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
-import {
-    getDatabase
-} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-database.js";
+// firebase.js - Karni Pro - الإعداد الرسمي الموحد - v10.12.2
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD5KnoOye-Ns7RNQq9KqGNSmakYydIoLAw",
-  authDomain: "my-store-4f430.firebaseapp.com",
-  databaseURL: "https://my-store-4f430-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "my-store-4f430",
-  storageBucket: "my-store-4f430.firebasestorage.app",
-  messagingSenderId: "674007434765",
-  appId: "1:674007434765:web:c4005d096ab86167cc3744"
+  apiKey: "AIzaSyCLIzcO9i8QQ-YzYY5fIap6QNDsJDqgf60",
+  authDomain: "karnipromaroc.firebaseapp.com",
+  databaseURL: "https://karnipromaroc-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "karnipromaroc",
+  storageBucket: "karnipromaroc.firebasestorage.app",
+  messagingSenderId: "6746147726",
+  appId: "1:6746147726:web:6e3a2d29941ab93ef432df",
+  measurementId: "G-3Q1L0LC9TE"
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
-
-export { db };
+export const auth = getAuth(app);
+export const db = getDatabase(app);
+export default app;
